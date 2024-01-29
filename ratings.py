aff775d710ae8be1adb04be3b6ba43f2e46e2f99
Ryan D Gregory <ryang872@gmail.com>
2024-01-29 17:00:01 -0600

    clown

--- ratings.py
+++ ratings.py
@@ -18,13 +18,13 @@ from teams import nba_teams
 # For local testing
 # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-service = Service(executable_path="CHROMEDRIVER_PATH")
+# service = Service(executable_path="CHROMEDRIVER_PATH")
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(service=service, options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 
 
 
