5d9625c0be55d17a04bfb3d1d89ecff938aecec3
Ryan D Gregory <ryang872@gmail.com>
2024-01-30 10:31:06 -0600

    Update ratings.py

--- ratings.py
+++ ratings.py
@@ -16,7 +16,7 @@ options.add_argument("--headless=new")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 options.add_argument('--remote-debugging-pipe')
-options.addArguments("--disable-gpu")
+options.add_argument("--disable-gpu")
 
 driver = webdriver.Chrome(options=options)
 
